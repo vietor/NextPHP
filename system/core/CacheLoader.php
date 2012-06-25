ec1e9ff2dd0541c9fd122ee73948030bdf974e60
Vietor Liu <vietor.liu@gmail.com>
2012-06-25 18:00:17 +0800

    Add base code

--- system/core/CacheLoader.php
+++ system/core/CacheLoader.php
@@ -1,23 +1,26 @@
 <?php
 class CacheLoader {
 	public static function connect(){
-		$config=Config::getConfig('cache');		
+		$config=Config::getConfig('cache');	
+		$type=$config['type'];
 		
-		if($config['type']=='redis') {
+		if($type=='redis') {
 			if(!class_exists('CcRedis'))
 				require_once(BASEPATH.'libs/Cache/CcRedis.php');
 			return CcRedis::getInstance($config['host'], $config['port']);
 		}
-		else if($config['type']=='memcache') {
+		else if($type=='memcache') {
 			if(!class_exists('CcMemcache'))
 				require_once(BASEPATH.'libs/Cache/CcMemcache.php');
 			return CcMemcache::getInstance($config['host'], $config['port']);
 		}
-		else {
+		else if($type=='memcached') {
 			if(!class_exists('CcMemcached'))
 				require_once(BASEPATH.'libs/Cache/CcMemcached.php');
 			return CcMemcached::getInstance($config['host'], $config['port']);
 		}
+		else 
+			throw new Exception('Unsupport cache type {'.$type.'}');
 	}
 }
 ?>
--- system/core/DatabaseLoader.php
+++ system/core/DatabaseLoader.php
@@ -5,10 +5,16 @@ class DatabaseLoader {
 			require_once(BASEPATH.'libs/Database/DbConnection.php');
 		
 		$config=Config::getConfig('database');
-		if($config['type']=='mysql') {
+		$type=$config['type'];
+		
+		if($type=='mysql')
 			$dsn='mysql:dbname='.$config['dbname'].';host='.$config['host'].';port='.$config['port'].';charset='.$config['charset'];
-			return new DbConnection($dsn,$config['user'],$config['passwd']);
-		}
+		else if($type=='pgsql')
+			$dsn='pgsql:dbname='.$config['dbname'].';host='.$config['host'].';port='.$config['port'].';charset='.$config['charset'];
+		else
+			throw new Exception('Unsupport database type {'.$config['type'].'}');
+		
+		return new DbConnection($dsn,$config['user'],$config['passwd']);
 	}
 }
 ?>
