67fca6101612970df45b7efb65784ceade7e5804
Vietor Liu <vietor.liu@gmail.com>
2012-06-25 20:33:35 +0800

    Add base code

--- application/controller/HelloWorld.php
+++ application/controller/HelloWorld.php
@@ -1,7 +1,7 @@
 <?php
 class HelloWorld extends Controller {
 	public function Test($request, $reponse) {
-		$reponse.output('<HTML><BODY>'.__METHOD__.'</BODY></HTML>'');
+		$reponse->output('<HTML><BODY>'.__METHOD__.'</BODY></HTML>');
 	}
 }
 ?>
