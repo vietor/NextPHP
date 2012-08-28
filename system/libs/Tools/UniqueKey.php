831abca5143f3f39486eaf99c7bbc0c1b0183261
Vietor Liu <vietor.liu@gmail.com>
2012-08-28 23:24:23 +0800

    Add base code

--- system/libs/Tools/UniqueKey.php
+++ system/libs/Tools/UniqueKey.php
@@ -1,91 +1,30 @@
 <?php
+require_once('EasyCrypto.php');
+
 class UniqueKey {
-	private $mode;
+	private $crypto;
 	private $secret;
 
 	public function __construct($mode,$password) {
-		$this->mode=$mode;
+		$this->crypto=new EasyCrypto($mode);
 		$this->secret=$password;
 	}
 
-	private function isAES() {
-		return $this->mode=="aes";
-	}
-
-	public function generate($id) {
+	public function generate($id, $expire=0) {
 		$obj = new stdClass;
 		$obj->id = $id;
+		$obj->expire = $expire>0?time()+$expire:0;
 		$obj->uniqueid = mt_rand(0, 65535).uniqid();
-		if($this->isAES()) {
-			$cipher = MCRYPT_RIJNDAEL_256;
-			$mode   = MCRYPT_MODE_ECB;
-		}
-		else {
-			$cipher = MCRYPT_3DES;
-			$mode   = MCRYPT_MODE_ECB;
-		}
-		return $this->do_encrypt($cipher,$mode,json_encode($obj));
+		$this->crypto->encrypt($this->secret,json_encode($obj));
 	}
 
 	public function validate($key) {
-	if($this->isAES()) {
-			$cipher = MCRYPT_RIJNDAEL_256;
-			$mode   = MCRYPT_MODE_ECB;
-		}
-		else {
-			$cipher = MCRYPT_3DES;
-			$mode   = MCRYPT_MODE_ECB;
-		}
-		$json=$this->do_decrypt($cipher,$mode,$key);
+		$json=$this->crypto->decrypt($this->secret, $key);
 		if(!($obj=json_decode($json)))
 			return false;
+		if($obj->expire>0 && time()>$obj->expire)
+			return false;
 		return $obj->id;
-	}
-
-	public function do_encrypt($cipher,$mode,$value) {
-		return self::base64_url_encode(
-				trim(
-						mcrypt_encrypt(
-								$cipher,
-								$this->secret,
-								$value,
-								$mode,
-								mcrypt_create_iv(
-										mcrypt_get_iv_size(
-												$cipher,
-												$mode
-										),
-										MCRYPT_RAND
-								)
-						)
-				)
-		);
-	}
-
-	public function do_decrypt($cipher,$mode,$value) {
-		return trim(
-				mcrypt_decrypt(
-						$cipher,
-						$this->secret,
-						self::base64_url_decode($value),
-						$mode,
-						mcrypt_create_iv(
-								mcrypt_get_iv_size(
-										$cipher,
-										$mode
-								),
-								MCRYPT_RAND
-						)
-				)
-		);
-	}
-
-	public static function base64_url_encode($input) {
-		return strtr(rtrim(base64_encode($input), '='), '+/', '-_');
-	}
-
-	public static function base64_url_decode($input) {
-		return base64_decode(strtr($input, '-_', '+/'));
 	}
 }
 ?>
