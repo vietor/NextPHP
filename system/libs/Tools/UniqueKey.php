2dc46061b00011bb6f38003ba405e562935f9e20
Vietor Liu <vietor.liu@gmail.com>
2012-10-05 14:40:34 +0800

    Add base code

--- system/libs/Tools/UniqueKey.php
+++ system/libs/Tools/UniqueKey.php
@@ -12,19 +12,22 @@ class UniqueKey {
 		$this->expire=$expire;
 	}
 
-	public function generate($id, $expire=0) {
+	public function generate($id, $bind='', $expire=0) {
 		if($expire==0)
 			$expire=$this->expire;
 		$obj = new stdClass;
 		$obj->id = $id;
+		$obj->bind = $bind;
 		$obj->expire = $expire>0?time()+$expire:0;
 		$obj->uniqueid = mt_rand(0, 65535).uniqid();
 		return $this->crypto->encrypt($this->secret,json_encode($obj));
 	}
 
-	public function validate($key) {
+	public function validate($key, $bind='') {
 		$json=$this->crypto->decrypt($this->secret, $key);
 		if(!($obj=json_decode($json)))
+			return false;
+		if($obj->bind != $bind)
 			return false;
 		if($obj->expire>0 && time()>$obj->expire)
 			return false;
