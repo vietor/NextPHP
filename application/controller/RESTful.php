a8b470c8ab2fe6249c99e7b782aa78679ef28eb1
Vietor Liu <vietor.liu@gmail.com>
2012-10-08 00:07:24 +0800

    Add a demo file for RESTful

---
+++ application/controller/RESTful.php
@@ -0,0 +1,27 @@
+<?php
+class RESTful extends Controller {
+	private static $errorMessage = array(
+			100001 => 'Some parameter is missing or bad length',
+			100002 => 'Perharps Database or Memcache has wrong',
+	);
+
+	protected function resultFailed($code) {
+		$result=array();
+		$result['error'] = $code;
+		$result['description'] = self::$errorMessage[$code];
+		$this->reponse->output(json_encode($result), 'application/json');
+		$this->exitProcess();
+	}
+
+	protected function resultSuccessed($result) {
+		$this->reponse->output(json_encode($result,true), 'application/json');
+	}
+
+	protected function getParamOrFailed($param,$minLen=0) {
+		if(!$this->request->hasParam($param,$minLen))
+			$this->resultFailed(100001);
+		else
+			return $this->request->getParam($param);
+	}
+}
+?>
