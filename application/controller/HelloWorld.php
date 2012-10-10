355608cc36411e5837ceafa1ee99cf7b19aee52a
Vietor Liu <vietor.liu@gmail.com>
2012-10-10 22:30:04 +0800

    Add base code

--- application/controller/HelloWorld.php
+++ application/controller/HelloWorld.php
@@ -1,7 +1,7 @@
 <?php
 class HelloWorld extends NpController {
 	public function Test() {
-		$hostUrl=$this->request->getUrlByBackward(3);
+		$hostUrl=$this->request->getUrlByBackward();
 		$this->reponse->output('<HTML><BODY><center>'.__METHOD__.'</center><p><p><center><img src="'.$hostUrl.'php-power-black.gif"></center></BODY></HTML>');
 	}
 }
