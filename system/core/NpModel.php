d28abd276c8c98d5cd78027d1adb6fad444b69c9
Vietor Liu <vietor.liu@gmail.com>
2012-10-26 14:32:27 +0800

    Add base code

--- system/core/NpModel.php
+++ system/core/NpModel.php
@@ -4,7 +4,7 @@ class NpModel {
 	private static $_models=array();
 
 	public static function loadModel($name) {
-		if(!array_key_exists($name,self::$_models)) {
+		if(!isset(self::$_models[$name])) {
 			if(!class_exists($name)){
 				require_once(NP_BASEPATH.'application/model/'.$name.'.php');
 				if(!class_exists($name))
