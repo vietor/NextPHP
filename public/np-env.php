a6f88437087e850d1a99ae942c61daeb722c77ad
Vietor Liu <vietor.liu@gmail.com>
2012-12-18 21:41:38 +0800

    mod: load environment code

--- public/np-env.php
+++ public/np-env.php
@@ -1,8 +1,10 @@
 <?php
-/* You can modify NP_SYS_PATH for share framework code */
-defined('NP_SYS_PATH') or define('NP_SYS_PATH', dirname(dirname(__FILE__)).'/system/');
+/* You can modify for share framework code */
+define('NP_SYS_PATH', dirname(dirname(__FILE__)).'/system/');
 
-defined('NP_APP_PATH') or define('NP_APP_PATH', dirname(dirname(__FILE__)).'/application/');
+/* You can modify for share logic code */
+define('NP_APP_PATH', dirname(dirname(__FILE__)).'/application/');
 
-class_exists("NpFramework") or require_once NP_SYS_PATH.'core/NpFramework.php';
+/* Load framework environment */
+require_once NP_SYS_PATH.'core/NpFramework.php';
 ?>
--- system/core/NpFramework.php
+++ system/core/NpFramework.php
@@ -39,15 +39,15 @@ class NpFramework
 {
 	private function handleRequest()
 	{
-		if(empty($_GET[NP_REQUEST_KEY]))
+		if(empty($_GET[NP_REDIRECT_KEY]))
 			throw new NpCoreException();
 
 		$urlArray=array();
-		$urlArray=explode('/',$_GET[NP_REQUEST_KEY]);
+		$urlArray=explode('/',$_GET[NP_REDIRECT_KEY]);
 		$urlArraySize=count($urlArray);
 		if($urlArraySize<2)
 			throw new NpCoreException();
-		unset($_GET[NP_REQUEST_KEY], $_REQUEST[NP_REQUEST_KEY]);
+		unset($_GET[NP_REDIRECT_KEY], $_REQUEST[NP_REDIRECT_KEY]);
 
 		$module=$urlArray[0];
 		$action=$urlArray[1];
@@ -85,8 +85,8 @@ class NpFramework
 		if(self::$instance!==null)
 			throw new NpCoreException('Framework multiple entry');
 
+		define('NP_REDIRECT_KEY','url');
 		set_exception_handler('NpExceptionHandler');
-		defined('NP_REQUEST_KEY') or define('NP_REQUEST_KEY','url');
 
 		if (get_magic_quotes_gpc ()) {
 			$in = array (&$_GET, &$_POST, &$_COOKIE, &$_FILES );
