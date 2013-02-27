f3fa5184ee8c810102c72dae7aa10d477296731a
Vietor Liu <vietor.liu@gmail.com>
2013-02-27 09:05:54 +0800

    add: mainpage for configuration

--- system/core/NpConfig.php
+++ system/core/NpConfig.php
@@ -1,4 +1,48 @@
 <?php
+
+/*! @mainpage Configuration
+ *
+ * \section intro Introduction
+ *
+ * You can create file <b>application/config.phpfile for configuration.
+ *
+ * \section intro Configuration items
+ *
+ * \subsection cookie 1. COOKIE
+ * For NpResponse::setCookie
+ * @param $_CONFIG['cookie']['domain']   cookie's domain, default $_SERVER['SERVER_NAME']
+ * @param $_CONFIG['cookie']['path']     cookie's path, default /
+ * @param $_CONFIG['cookie']['timeout']  seconds for cookie's expire time, default 2 week
+ *
+ * \subsection encryptor 2. Encrypt
+ * For NpFactory::getEncryptor
+ * @param $_CONFIG['encryptor']['mode']     encrypt mode, support aes, 3des; default aes
+ * @param $_CONFIG['encryptor']['password'] password, length limit: min 7, max 32 for aes and 16 for 3des
+ * @param $_CONFIG['encryptor']['timeout']  seconds for expire time, default 0 (forever)
+ * 
+ * \subsection database 3. Database
+ * For NpFactory::getDatabase
+ * @param $_CONFIG['database']['type']    database type for PDO, default mysql
+ * @param $_CONFIG['database']['host']    host or ip address for connect, default localhost
+ * @param $_CONFIG['database']['port']    port for connect, default 3306
+ * @param $_CONFIG['database']['user']    username for connect, default root
+ * @param $_CONFIG['database']['passwd']  password for connect, default empty
+ * @param $_CONFIG['database']['dbname']  database name for connect, default mysql
+ * @param $_CONFIG['database']['charset'] charset for connect, default utf8
+ * 
+ * For NpFactory::getExtraDatabase, $_CONFIG's key is 'database-' append extra name.
+ * 
+ * \subsection cache 4. Key-Value Cache
+ * For NpFactory::getCache
+ * @param $_CONFIG['cache']['type']    cache type, support memcache,memcached,redis; default memcache
+ * @param $_CONFIG['cache']['host']    host or ip address for connect, default localhost
+ * @param $_CONFIG['cache']['port']    port for connect, default 11211
+ * @param $_CONFIG['cache']['prefix']  prefix append for keys, default empty
+ * @param $_CONFIG['cache']['timeout'] seconds for expire time, default 0 (forever)
+ * 
+ * For NpFactory::getExtraCache, $_CONFIG's key is 'cache-' append extra name.
+ */
+
 //! The class for configs
 class NpConfig {
 	private static $configs;
