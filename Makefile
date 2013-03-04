c10cab9db22d21f63df4eff9647cef816f696d66
Vietor Liu <vietor.liu@gmail.com>
2013-03-04 14:49:02 +0800

    mod: use make file for document & compress

--- Makefile
+++ Makefile
@@ -5,13 +5,15 @@ TARGET=NextPHP
 all: doc
 
 doc:
-	doxygen
+	doxygen tools/Doxyfile
 	$(MAKE) -C latex
 	cp latex/refman.pdf $(TARGET).pdf
 	tar -czf $(TARGET)-html.tar.gz html
+	php tools/compress.php ./system $(TARGET).php
 
 clean:
 	rm -fr html latex
 	rm -f *.log
+	rm -f $(TARGET).php
 	rm -f $(TARGET).pdf
 	rm -f $(TARGET)-html.tar.gz
---
+++ tools/compress.php
@@ -0,0 +1,93 @@
+<?php
+function compress_to_string($source) {
+	$skip2Include=true;
+	$skip4Include=false;
+	$needCloseTag=false;
+	$compressed = null;
+	$tokens = token_get_all($source);
+	foreach ($tokens as $token) {
+		if (!is_array($token)) {
+			if($skip4Include)
+				$skip4Include=false;
+			else
+				$compressed .= $token;
+		} else {
+			switch ($token[0]) {
+				case T_INLINE_HTML:
+				case T_COMMENT:
+				case T_DOC_COMMENT:
+					break;
+				case T_WHITESPACE:
+				case T_ENCAPSED_AND_WHITESPACE:
+					$compressed .= ' ';
+					break;
+				case T_INCLUDE:
+				case T_INCLUDE_ONCE:
+				case T_REQUIRE:
+				case T_REQUIRE_ONCE:
+					if($skip2Include)
+						$skip4Include=true;
+					else
+						$compressed .= $token[1];
+					break;
+				case T_OPEN_TAG:
+					break;
+				case T_CLOSE_TAG:
+					if($needCloseTag)
+					{
+						$needCloseTag=false;
+						$compressed .= '?>';
+					}
+					break;
+				case T_OPEN_TAG_WITH_ECHO:
+					$needCloseTag=true;
+					$compressed .= '<?php echo ';
+					break;
+				default:
+					if(!$skip4Include)
+					{
+						$skip2Include=false;
+						$compressed .= sprintf('%s', $token[1]);
+					}
+					break;
+			}
+		}
+	}
+	return trim($compressed);
+}
+
+function compress_one($from, $to) {
+	$result = array();
+	$result['file_size']=0;
+	$result['file_list']=array();
+	$result['compressed_size']=0;
+	$dir_list = array($from);
+	file_put_contents($to, "<?php\n");
+	while (count($dir_list) > 0) {
+		$files = glob(array_pop($dir_list) . '/*');
+		foreach ($files as $path) {
+			if (is_file($path)) {
+				if (strrpos($path, '.php', -4) > 0) {
+					$result['file_list'][]=$path;
+					$code = file_get_contents($path);
+					$result['file_size'] += strlen($code);
+					$code = compress_to_string($code)."\n";
+					$result['compressed_size'] += strlen($code);
+					file_put_contents($to, $code, FILE_APPEND);
+				}
+			} else {
+				$dir_list[] = $path;
+			}
+		}
+	}
+	file_put_contents($to, "?>", FILE_APPEND);
+	return $result;
+}
+
+$result=compress_one($argv[1],$argv[2]);
+echo 'source size:'.$result['file_size'].', compressed size:'.$result['compressed_size']."\n";
+echo 'compression ratio:'.round(($result['compressed_size']/$result['file_size'])*100, 2)."%, files:\n";
+foreach ($result['file_list'] as $path) {
+	echo $path."\n";
+}
+?>
